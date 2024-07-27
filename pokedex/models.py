a3f92b1f01a7dddb4817e663c3fd32131c0d1e5c
Williams Andres Sierra Narvaez <wasierran@puce.edu.ec>
2024-07-26 22:51:43 -0500

    audios

--- pokedex/models.py
+++ pokedex/models.py
@@ -7,8 +7,9 @@ class Pokemon(models.Model):
     type = models.CharField(max_length=100)
     height = models.FloatField()
     weight = models.FloatField()
-    attack = models.CharField(max_length=100, default='Tackle')  # Define a default value
+    attack = models.CharField(max_length=100, default='Tackle')
     image_url = models.CharField(max_length=200, default='')
+    audio_url = models.CharField(max_length=200, default='')  # MP3
 
     def __str__(self):
         return self.name
